5c75253d6effdd93c1cd05bda2d8e65ecd08d48e
John Bannister <johnb@hey.com>
2022-11-07 21:27:17 +0000

    Add GetPosts endpoint

--- content/posts_test.go
+++ content/posts_test.go
@@ -11,6 +11,8 @@ import (
 )
 
 func TestCreatePost(t *testing.T) {
+	setupDB(t)
+
 	res, err := content.CreatePost(context.Background(), &content.CreatePostParams{
 		Slug:    "a-test-post",
 		Title:   "A test post",
@@ -22,11 +24,43 @@ func TestCreatePost(t *testing.T) {
 	}
 
 	if diff := cmp.Diff(&content.CreatePostResponse{
-		Slug:      "a-test-post",
-		Title:     "A test post",
-		Summary:   "Just verifying that creating a post works.",
-		Body:      "Sometimes it's worth being careful. Adding tests is a great way to do that.\n\nEspecially if they're run automatically!",
-		CreatedAt: time.Now(),
+		Post: content.Post{
+			Slug:      "a-test-post",
+			Title:     "A test post",
+			Summary:   "Just verifying that creating a post works.",
+			Body:      "Sometimes it's worth being careful. Adding tests is a great way to do that.\n\nEspecially if they're run automatically!",
+			CreatedAt: time.Now(),
+		},
+	}, res, cmpopts.EquateApproxTime(time.Second)); diff != "" {
+		t.Error(diff)
+	}
+}
+
+func TestGetPosts(t *testing.T) {
+	setupDB(t)
+
+	_, err := content.CreatePost(context.Background(), &content.CreatePostParams{
+		Slug:    "a-test-post",
+		Title:   "A test post",
+		Summary: "Just verifying that creating a post works.",
+		Body:    "Sometimes it's worth being careful. Adding tests is a great way to do that.\n\nEspecially if they're run automatically!",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res, err := content.GetPosts(context.Background())
+
+	if diff := cmp.Diff(&content.GetPostsResponse{
+		Posts: []content.Post{
+			{
+				Slug:      "a-test-post",
+				Title:     "A test post",
+				Summary:   "Just verifying that creating a post works.",
+				Body:      "Sometimes it's worth being careful. Adding tests is a great way to do that.\n\nEspecially if they're run automatically!",
+				CreatedAt: time.Now(),
+			},
+		},
 	}, res, cmpopts.EquateApproxTime(time.Second)); diff != "" {
 		t.Error(diff)
 	}
